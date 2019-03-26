a7ee74b0fc89d7fd25f42423bcc696ebbcdf52cf
Kamaludin <kamal.zyel@gmail.com>
2019-03-26 17:10:22 +0700

    Update db_berry.sql
    Fix error name database where import at mysql

--- databases/db_berry.sql
+++ databases/db_berry.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `epiz_21328375_candys`
+-- Database: `db_berry`
 -
 
 -- --------------------------------------------------------
